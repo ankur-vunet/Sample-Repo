5fd9e1a5a852e7c92a0c3634b1e2e9cd79f345b5
ankur-vunet <107390060+ankur-vunet@users.noreply.github.com>
2024-07-29 17:31:44 +0530

    index base url commit

---
+++ assets/js/f6bcff26.20d25a68.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[253],{8732:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Sample-Repo/index.html/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
